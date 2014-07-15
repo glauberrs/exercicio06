526a20aaea31adfb0494248a082f3ae184905925
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2014-07-15 18:15:02 -0300

    Initial Commit

---
+++ exercicio06/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  exercicio06
+//
+//  Created by Jackson F. de A. Mafra on 15/07/2014.
+//  Copyright (c) 2014 Jackson F. de A. Mafra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
